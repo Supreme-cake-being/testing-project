d3d568e9baf85e8e0d7e9e52da17700c3ce85485
Supreme-cake-being <XGon20987@gmail.com>
2025-08-12 13:43:47 +0300

    added tests to asynchronous code

--- async-project/async/async-example.spec.js
+++ async-project/async/async-example.spec.js
@@ -1,1 +1,27 @@
-it
+import { describe, expect, it } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
+
+describe("generateToken", () => {
+  it("should generate a token value", (done) => {
+    const testEmail = "test@test.com";
+
+    generateToken(testEmail, (err, token) => {
+      try {
+        expect(token).toBeDefined();
+        done();
+      } catch (error) {
+        done(err);
+      }
+    });
+  });
+});
+
+describe("generateTokenPromise", () => {
+  it("should generate a token value", async () => {
+    const testEmail = "test@test.com";
+
+    const token = await generateTokenPromise(testEmail);
+
+    expect(token).toBeDefined();
+  });
+});
