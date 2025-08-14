f39bb89c733f4c0ab0d47cd56825087b4a3c2ee9
Karr4 but from another universe <XGon20987@gmail.com>
2025-08-14 20:15:41 +0300

    added tests for TermAndConditions

---
+++ react-testing/tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
+
+describe("TermsAndConditions", () => {
+  it("should render with correct text and initial state", () => {
+    render(<TermsAndConditions />);
+
+    const h1 = screen.getByRole("heading");
+    expect(h1).toBeInTheDocument();
+    expect(h1).toHaveTextContent("Terms & Conditions");
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  it("should enble the button if the checkbox is checked", async () => {
+    render(<TermsAndConditions />);
+
+    const checkbox = screen.getByRole("checkbox");
+
+    const user = userEvent.setup();
+    await user.click(checkbox);
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toBeChecked();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).not.toBeDisabled();
+  });
+});
